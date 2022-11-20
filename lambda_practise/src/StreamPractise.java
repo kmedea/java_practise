f03d3ce505d4f8a30b17826c4a9b06dfba7b202e
kmedea <kerklecz.medea@gmail.com>
2022-11-20 15:34:29 +0100

    1 - 11. feladat done

--- lambda_practise/src/StreamPractise.java
+++ lambda_practise/src/StreamPractise.java
@@ -1,3 +1,5 @@
+import com.sun.xml.internal.ws.resources.StreamingMessages;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -5,6 +7,7 @@ import java.nio.file.Paths;
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StreamPractise {
 
@@ -187,5 +190,72 @@ public class StreamPractise {
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
     }
 
+    /*A swcharacters.csv fájl tartalmazza a Star Wars univerzumában megjelenő karaktereket.
 
+Írj Stream kifejezéseket, ami elvégzik az alább feladatokat:
+
+    Írasd ki a legnehezebb karakter nevét (ha a tömege ismeretlen, hagyja figyelmen kívül)
+    Írja ki a férfi karakterek átlag magasságát
+    Írja ki a női karakterek átlag magasságát
+    Írasd ki a karakterek nem szerinti kormegoszlását (ahol a nem lehet "male", "female", "other")
+        Az életkor csoportok : "below 21", "between 21 and 40", "above 40" és "unknown"
+        Az eredmény legyen egy Map<String, Map<String, Integer>> */
+
+    public void theHeaviestCharacter(String fileName) {
+
+        List<String[]> fileContent = readFile(fileName);
+
+        int maxHeavy = fileContent.stream()
+                .filter(f -> f[2].matches("\\d+$"))
+                .map(f -> Integer.valueOf(f[2]))
+                .mapToInt(f -> f)
+                .max()
+                .orElse(Integer.MIN_VALUE);
+
+        String heaviestCharacter = fileContent.stream()
+                .filter(f -> f[2].matches("\\d+$"))
+                .filter(f -> Integer.parseInt(f[2]) == maxHeavy)
+                .map(f -> f[0])
+                .findFirst()
+                .orElse("kismalac");
+
+        System.out.println(heaviestCharacter);
+    }
+
+    public void avgHeight(String fileName, String gender){
+
+        List<String[]> fileContent = readFile(fileName);
+
+        double avgHeight =
+                fileContent.stream()
+                        .filter(f -> f[1].matches("\\d+$"))
+                        .filter(f -> f[7].equals(gender))
+                        .mapToInt(f -> Integer.parseInt(f[1]))
+                        .average()
+                        .orElse(0.0);
+
+        System.out.println(avgHeight);
+    }
+
+    private List<String[]> readFile(String fileName){
+
+        List<String[]> fileContent;
+        Path path = Paths.get(fileName);
+
+        try {
+            fileContent = Files.readAllLines(path).stream().skip(1).map(str -> str.split(";")).collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new IllegalStateException("A fájl beolvasása nem sikerült", e);
+        }
+
+        return fileContent;
+    }
+
+    public static void main(String[] args) {
+        StreamPractise streamPractise = new StreamPractise();
+        streamPractise.theHeaviestCharacter("swcharacters.csv");
+        streamPractise.avgHeight("swcharacters.csv", "male");
+        streamPractise.avgHeight("swcharacters.csv", "female");
+
+    }
 }
